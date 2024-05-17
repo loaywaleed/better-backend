1967ea0e2e7ba90b13dd6250ddc0b3f8bed959d5
loaywaleed <loaywaleed9@gmail.com>
2024-05-17 16:24:36 +0300

    initial doc

--- api/communities/models.py
+++ api/communities/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Community(models.Model):
+    """
+    Model representing communities in the app
+    """
     name = models.CharField(max_length=250)
     icon = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
