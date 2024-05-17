2ac7438abada7ea587837e5c72a20035cb709615
loaywaleed <loaywaleed9@gmail.com>
2024-05-17 16:11:42 +0300

    adding relationship between users and communities

--- api/communities/models.py
+++ api/communities/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Community(models.Model):
     name = models.CharField(max_length=250)
     icon = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
+    members = models.ManyToManyField(User)
 
     def __str__(self) -> str:
         return self.name
--- api/communities/serializers.py
+++ api/communities/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Community
+from ..authentication.serializers import UserSerializer
 
 
 class CommunitySerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True, read_only=True)
     class Meta:
         model = Community
         fields = "__all__"
