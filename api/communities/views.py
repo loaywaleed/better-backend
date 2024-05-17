52b9b9435e1ec897ea8cfc9f4741dc71ced8ea01
loaywaleed <loaywaleed9@gmail.com>
2024-05-17 16:39:29 +0300

    protecting community create route

--- api/communities/views.py
+++ api/communities/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Community
 from .serializers import CommunitySerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class CommunityListCreate(generics.ListCreateAPIView):
     queryset = Community.objects.all()
     serializer_class = CommunitySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
