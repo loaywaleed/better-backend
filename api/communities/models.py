e1293f69f4fc3bbe09ab817400e42506d3d56139
loaywaleed <loaywaleed9@gmail.com>
2024-05-29 21:26:49 +0300

    Community's habits model

--- api/communities/models.py
+++ api/communities/models.py
@@ -13,3 +13,15 @@ class Community(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+class CommunityHabit(models.Model):
+    """
+    Model representing community habits
+    """
+    name = models.CharField(max_length=250)
+    icon = models.CharField(max_length=100, null=True)
+    description = models.TextField(null=True)
+    frequency = models.IntegerField(null=True) # Number of days to do the habit per week
+    motivation = models.TextField(null=True)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+    
